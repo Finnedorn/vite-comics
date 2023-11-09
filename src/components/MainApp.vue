409d5d2b55283920179ea3cf7ac9b5e3f43bbd17
Finnedorn <daniele.giuntoli@outlook.com>
2023-11-09 22:33:27 +0100

    added main + bonus section

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -9,10 +9,13 @@
         </div>
         <div class="bg-my-light-blue">
             <div class="container py-5">
-                <ul>
+                <ul class="d-flex flex-row flex-wrap justify-content-between align-items-center align-content-center  ">
                     <li v-for="(el) in mainImgArr" class="me-4">
-                        <a href="#">
-                            <img :src="el" alt="">
+                        <a href="#" class="text-decoration-none d-flex align-items-center justify-content-center">
+                            <div class="img-wrapper">
+                                <img :src="el.picture" :alt="el.info">
+                            </div>
+                            <h3 class="font-my-light fs-6 ms-3">{{el.title}}</h3>
                         </a>
                     </li>
                 </ul>
@@ -27,10 +30,32 @@ export default {
     data() {
         return {
             mainImgArr: [
-                "/public/img/buy-comics-digital-comics.png",
-                '/public/img/buy-comics-merchandise.png',
-                '/public/img/buy-comics-subscriptions.png',
-                '/public/img/buy-comics-shop-locator.png'
+                {
+                    picture:"/public/img/buy-comics-digital-comics.png",
+                    info:"buy-comics-digital-comics",
+                    title:"DIGITAL COMICS"
+
+                },
+                {
+                    picture:"/public/img/buy-comics-merchandise.png",
+                    info:"buy-comics-merchandise",
+                    title:"DC MERCHANDISE"
+                },
+                {
+                    picture:"/public/img/buy-comics-subscriptions.png",
+                    info:"buy-comics-subscriptions",
+                    title:"SUBSCRIPTION"
+                },
+                {
+                    picture:"/public/img/buy-comics-shop-locator.png",
+                    info:"buy-comics-shop-locator",
+                    title:"COMICS SHOP LOCATOR"
+                },
+                {
+                    picture: "/public/img/buy-dc-power-visa.svg",
+                    info:"buy-dc-power-visa",
+                    title:"DC POWER VISA"
+                }
             ],
         }
     }
@@ -46,5 +71,29 @@ export default {
 }
 .font-my-light {
     color: var(--light);
+}
+
+ul{
+    list-style: none;
+}
+
+img {
+    width: 100%;
+}
+.img-wrapper {
+    width: 50px;
+    height: 50px;
+}
+
+ul>li:first-child>a>div {
+    width: 40px;
+    height: 40px;
+    margin-bottom: 10px;
+}
+
+ul>li:nth-child(4)>a>div {
+    width: 35px;
+    height: 35px;
+    margin-bottom: 20px;
 }
 </style>
